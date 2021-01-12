afb81264eb64c0463ff00bd5bf3b4ad8f5770ba5
anytochka <anytakulik@gmail.com>
2021-01-12 13:41:59 +0200

    first commit

---
+++ migration/ExamPractice/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace ExamPractice
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }
+    }
+    public class Doctor : Employee
+    {
+        public string Specialization { get; set; }
+    }
+    public class Engineer : Employee
+    {
+        public string FavoriteVideogame { get; set; }
+    }
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+    public class Context : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Engineer> Engineers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Employee>().HasRequired(x => x.City).WithMany(x => x.Employees).HasForeignKey(x => x.CityId);
+
+            modelBuilder.Entity<Doctor>().Property(d => d.Specialization).IsRequired();
+            modelBuilder.Entity<Employee>().Property(e => e.Name).HasMaxLength(128);
+        }
+    }
+
+
+    class Program
+    {
+        public static IEnumerable<string> GetSpecialization(string cityName)
+        {
+            using (var ctx = new Context())
+            {
+                return ctx.Doctors.Where(d => d.CityId == ctx.Cities.FirstOrDefault(x=>x.Name == cityName).Id)
+                    .Select(d => d.Specialization).ToList();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ threads/Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the count of threads: ");
+            int.TryParse(Console.ReadLine(), out var maxThreads);
+            ThreadPool.SetMaxThreads(maxThreads, maxThreads);
+            var watch = Stopwatch.StartNew();
+
+            var tasks = new Task[maxThreads];
+            for (var i = 0; i < maxThreads; i++)
+                tasks[i] = Task.Run(() => Job());
+
+            Task.WaitAll(tasks);
+            watch.Stop();
+            Console.WriteLine("Time: " + watch.ElapsedMilliseconds + "ms");
+            Console.ReadLine();
+        }
+
+        public static void Job()
+        {
+            while (count < 100)
+            {
+                count++;
+                var simpleNums = new List<int>();
+                foreach (var i in Enumerable.Range(1, 10000))
+                    if (IsPrime(i))
+                        simpleNums.Add(i);
+            }
+        }
+
+        public static bool IsPrime(int n)
+        {
+            var result = true;
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+
+}
